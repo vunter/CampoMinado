e1b93912f00c0dbdbd645c6ef078f0427a907123
leonardo.catanante <leonardo.catanante@AN083102.anvisa.gov.br>
2018-08-07 11:22:06 -0300

    Adicionado dificuldade Médio, alteração nº de minas e matriz difícil

--- CampoMinado/src/campoMinadoAPS/CodJogoPrincipal.java
+++ CampoMinado/src/campoMinadoAPS/CodJogoPrincipal.java
@@ -1,8 +1,9 @@
 package campoMinadoAPS;
 
 public class CodJogoPrincipal {
-	private JogoDificil jogo;
+	private JogoDificil jogoDificil;
 	private JogoFacil jogoFacil;
+	private JogoMedio jogoMedio;
 	boolean terminar = false;
 	boolean ganhou = false;
 	int rodada = 0;
@@ -12,13 +13,40 @@ public class CodJogoPrincipal {
 			jogoFacil = new JogoFacil();
 			Jogar(jogoFacil);
 		} else if (dificuldade == 2) {
-			jogo = new JogoDificil();
-			Jogar(jogo);
+			jogoMedio = new JogoMedio();
+			Jogar(jogoMedio);
+		} else if (dificuldade == 3) {
+			jogoDificil = new JogoDificil();
+			Jogar(jogoDificil);
 		}
 		
 	}
 
-	public void Jogar(JogoDificil jogo) {
+
+	public void Jogar(JogoFacil jogo) {
+		do {
+			rodada++;
+			System.out.println("Rodada " + rodada);
+			jogo.exibe();
+			terminar = jogo.setPosicao();
+
+			if (!terminar) {
+				jogo.abrirProximas();
+				terminar = jogo.ganhou();
+			}
+
+		} while (!terminar);
+
+		if (!jogo.ganhou()) {
+			System.out.println("BUUUM! Você perdeu!");
+			jogo.exibeMinas();
+		} else {
+			System.out.println("Parabéns, você deixou os 8 campos de minas livres em " + rodada + " rodadas");
+
+			jogo.exibeMinas();
+		}
+	}
+	public void Jogar(JogoMedio jogo) {
 		do {
 			rodada++;
 			System.out.println("Rodada " + rodada);
@@ -41,7 +69,7 @@ public class CodJogoPrincipal {
 			jogo.exibeMinas();
 		}
 	}
-	public void Jogar(JogoFacil jogo) {
+	public void Jogar(JogoDificil jogo) {
 		do {
 			rodada++;
 			System.out.println("Rodada " + rodada);
@@ -59,7 +87,7 @@ public class CodJogoPrincipal {
 			System.out.println("BUUUM! Você perdeu!");
 			jogo.exibeMinas();
 		} else {
-			System.out.println("Parabéns, você deixou os 8 campos de minas livres em " + rodada + " rodadas");
+			System.out.println("Parabéns, você deixou os 20 campos de minas livres em " + rodada + " rodadas");
 
 			jogo.exibeMinas();
 		}
